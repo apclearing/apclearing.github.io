0f68f1054ad0ee9bfbfc7abb6f48d84346eadb02
Scotty Carlson <scottythered@gmail.com>
2017-07-02 21:46:19 -0500

    Update for Python and Filters

---
+++ filters.py
@@ -0,0 +1,5 @@
+﻿with open('www/index.html', 'r+') as f:
+    content = f.read()
+    f.seek(0)
+    f.truncate()
+    f.write(content.replace('<p class="center"><strong>placeholder</strong></p></section>', '</section><section class="row-fluid clearfix hidden-xs hidden-sm"><div class="center"><div class="btn-group btn-group-justified"><a href="#" data-filter="all" class="btn btn-info">All Mapping Documents</a><a href="#" data-filter="application-profile" class="btn btn-default">Application Profiles</a><a href="#" data-filter="mapping" class="btn btn-default">Mappings</a><a href="#" data-filter="code" class="btn btn-default">Code</a></div></div></section><br>'))
